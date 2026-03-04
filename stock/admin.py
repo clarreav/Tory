a2c71ef54cedf7d38f7ff0c4f9900cf1f12d2877
Christian Larrea <57969291+BlackSkyFrost@users.noreply.github.com>
2026-03-04 12:33:18 -0500

    models update for new fields for Producto class

--- stock/admin.py
+++ stock/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
-from .models import Producto
+from .models import Producto, Categoria
 
-admin.site.register(Producto)
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    # Columnas que verás en la lista principal
+    list_display = ('sku', 'nombre', 'cantidad', 'creado_por')
+
+    # Función mágica: asigna el usuario actual al guardar
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:  # Si el producto es nuevo (no tiene ID)
+            obj.creado_por = request.user
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Categoria)
--- stock/models.py
+++ stock/models.py
@@ -1,10 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Producto(models.Model):
-    nombre = models.CharField(max_length=100)
-    cantidad = models.IntegerField(default=0)
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
-    descripcion = models.TextField(blank=True)
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nombre
+
+class Producto(models.Model):
+    sku = models.CharField("Código Único (SKU)", max_length=20, unique=True)
+    nombre = models.CharField(max_length=100)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True)
+    stock_minimo = models.PositiveIntegerField("Alerta de Stock Mínimo", default=5)
+    cantidad = models.IntegerField(default=0)
+    costo = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    pvp = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    descripcion = models.TextField(blank=True)
+    creado_el = models.DateTimeField(auto_now_add=True)
+    actualizado_el = models.DateTimeField(auto_now=True)
+    activo = models.BooleanField(default=True)
+    creado_por = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, editable=False)
+
+    def __str__(self):
+        return f"{self.sku} - {self.nombre}"
