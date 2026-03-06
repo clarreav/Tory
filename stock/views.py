d7a45a9a55ec500d375ee21092a9d73fd96bf937
Christian Larrea <57969291+BlackSkyFrost@users.noreply.github.com>
2026-03-06 11:36:33 -0500

    update views for Product creation and product list

--- stock/views.py
+++ stock/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Producto
+from .forms import ProductoForm
 
-# Create your views here.
+def lista_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'stock/lista.html', {'productos': productos})
+
+def crear_producto(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            producto = form.save(commit=False)
+            producto.creado_por = request.user # Asigna al usuario actual
+            producto.save()
+            return redirect('lista') # Redirige a la tabla
+    else:
+        form = ProductoForm()
+    return render(request, 'stock/crear.html', {'form': form})
